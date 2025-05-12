b040a9adba0ff2bf4a39d70ff28b2a7565ab1fde
Eddy Chang <eddy@joomla.com.tw>
2025-05-12 15:48:58 +0800

    函式流程控制-計數器超過10

--- js-0512/ex1-1/index.js
+++ js-0512/ex1-1/index.js
@@ -3,11 +3,12 @@
 // 以下是用selector(選取器)語法(對應css技術)
 const count = document.querySelector('#count')
 
-count = 1
-
 // 加入事件監聽器
 count.addEventListener('click', function () {
+  if (Number(count.innerText) > 10) {
+    alert('超過10了，不能再加!')
+    return
+  }
   // 當 count 被點擊時，將 count 的數字加 1
   count.innerText = Number(count.innerText) + 1
 })
-
