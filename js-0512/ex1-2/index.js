218d54b37f8879cbd9c13593bb98e4819fb049e4
Eddy Chang <eddy@joomla.com.tw>
2025-05-12 14:45:27 +0800

    +註解

--- js-0512/ex1-2/index.js
+++ js-0512/ex1-2/index.js
@@ -5,12 +5,13 @@ const total = document.querySelector('#total')
 
 // 加入事件監聽器(click事件是"點按滑鼠")
 increase.addEventListener('click', function () {
-  // 將 total 的數字+1
+  // 先將 total 的文字內容轉為數字，然後加 1，最後更新回 total 的文字內容
   total.innerText = Number(total.innerText) + 1
 })
 
 // 加入事件監聽器(click事件是"點按滑鼠")
 decrease.addEventListener('click', function () {
-  // 將 total 的數字-1
+  // 先將 total 的文字內容轉為數字，然後減 1，最後更新回 total 的文字內容
   total.innerText = Number(total.innerText) - 1
 })
+
