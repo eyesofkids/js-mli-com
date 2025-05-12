62d99aaae99ca67bb2d4320cc476941b1b54f2ae
Eddy Chang <eddy@joomla.com.tw>
2025-05-12 14:25:40 +0800

    Update index.js

--- js-0512/ex1-1/index.js
+++ js-0512/ex1-1/index.js
@@ -1,5 +1,11 @@
-const count = document.getElementById('count')
+// 常數count得到的是DOM物件實體
+// const count = document.getElementById('count')
+// 以下是用selector(選取器)語法(對應css技術)
+const count = document.querySelector('#count')
 
+
+// 加入事件監聽器
 count.addEventListener('click', function () {
+  // 當 count 被點擊時，將 count 的數字加 1
   count.innerText = Number(count.innerText) + 1
 })
