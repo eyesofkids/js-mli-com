21304837deda3a20b129229c070867f1f79d7261
Eddy Chang <eddy@joomla.com.tw>
2025-05-12 14:40:28 +0800

    Update index.js

--- js-0512/ex1-2/index.js
+++ js-0512/ex1-2/index.js
@@ -1,14 +1,15 @@
+// 獲得DOM元素物件實體
 const increase = document.querySelector('#increase')
 const decrease = document.querySelector('#decrease')
 const total = document.querySelector('#total')
 
-// 加入事件監聽器
+// 加入事件監聽器(click事件是"點按滑鼠")
 increase.addEventListener('click', function () {
   // 將 total 的數字+1
   total.innerText = Number(total.innerText) + 1
 })
 
-// 加入事件監聽器
+// 加入事件監聽器(click事件是"點按滑鼠")
 decrease.addEventListener('click', function () {
   // 將 total 的數字-1
   total.innerText = Number(total.innerText) - 1
