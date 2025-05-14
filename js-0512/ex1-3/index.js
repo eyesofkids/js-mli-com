22fb4967f33790ab4575efd44cec273c793255d8
Eddy Chang <eddy@joomla.com.tw>
2025-05-14 10:59:24 +0800

    練習1-3_美金轉新台幣_撰寫轉換函式

--- js-0512/ex1-3/index.js
+++ js-0512/ex1-3/index.js
@@ -4,13 +4,32 @@ const usd2twd = document.querySelector('#usd2twd')
 const twd2usd = document.querySelector('#twd2usd')
 const result = document.querySelector('#result')
 
-// (1 USD 美金 = 30.31 TWD 新台幣)
+// 匯率公式: 1 USD 美金 = 30.42 TWD 新台幣
+const exchangeRate = 30.42
+
+// 從輸入的金額(字串) => 幾個小數點(digit)的字串 => 加入千位符的金額字串
+// type = 'multiply' | 'division'
+const converter = (value, rate, digit = 0, type = 'multiply') => {
+  // 先乘或除匯率
+  const a1 = type === 'multiply' ? Number(value) * rate : Number(value) / rate
+  // 調整小數點位數`(Number).toFixed(0)` 從數字回傳一個小數點後0位的"字串"
+  const a2 = Number(a1.toFixed(digit))
+  // 呈現千位符`(Number).toLocaleString()` 呈現時加入千位符號
+  return a2.toLocaleString()
+}
+
 // 美金 -> 新台幣
 usd2twd.addEventListener('click', function () {
   // input文字輸入框使用 `.value`得到輸入值
   // 從html中得到的值對js來說一定是字串值
-  // `toFixed(0)` 從數字回傳一個小數點後0位的"字串"
-  const cValue = (Number(money.value) * 30.31).toFixed(0)
-  // 呈現時加入千位符號
-  result.innerHTML = '轉換金額(新台幣): NT$' + Number(cValue).toLocaleString()
+  //   const cValue = (Number(money.value) * exchangeRate).toFixed(0)
+  // `(Number).toLocaleString()` 呈現時加入千位符號
+  result.innerHTML =
+    '轉換金額(新台幣): NT$' +
+    converter(money.value, exchangeRate, 0, 'multiply')
+})
+
+// 新台幣 -> 美金
+twd2usd.addEventListener('click', function () {
+  result.innerHTML = '轉換金額(美金): $' + converter(money.value, exchangeRate, 2, 'division')
 })
