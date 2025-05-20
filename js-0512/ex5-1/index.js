77b099b44d0cc3346c236ea5c9c459989796b761
Eddy Chang <eddy@joomla.com.tw>
2025-05-20 14:03:16 +0800

    練習5-呈現表格

--- js-0512/ex5-1/index.js
+++ js-0512/ex5-1/index.js
@@ -1,10 +1,31 @@
-//預設導入(用不同名稱可以直接改名稱，不建議改名稱使用)
-import sum from "./products.js";
-
-console.log(sum(1,2))
-
 // 命名導入(可以用as改名稱)
-// import { products as prods, brands } from "./products.js";
+import { products } from './products.js'
 
-// console.log(prods)
-// console.log(brands)
+// 以下獲取DOM各元素物件實體
+const result = document.querySelector('#result')
+
+// 呈現表格
+let display = `<table border="1">
+<thead>
+<tr>
+<th>ID</th>
+<th>名稱</th>
+<th>價格</th>
+</tr>
+</thead>
+<tbody>`
+
+display += products
+  .map((v, i) => {
+    return `<tr>
+            <td>${v.id}</td>
+            <td>${v.name}</td>
+            <td>${v.price}</td>
+         </tr>`
+  })
+  .join('')
+
+display += '</tbody></table>'
+
+result.innerHTML = display
+
