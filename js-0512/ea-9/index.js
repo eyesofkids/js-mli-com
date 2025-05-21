2753cb428d3ea80cd0a90b881e540f2b08c76efe
Eddy Chang <eddy@joomla.com.tw>
2025-05-21 10:47:10 +0800

    fetch範例_async-await語法

--- js-0512/ea-9/index.js
+++ js-0512/ea-9/index.js
@@ -1,6 +1,6 @@
 // 呈現資料用函式
 const display = (users) => {
-  const usersNode= users
+  const usersNode = users
     .map((user) => {
       return `<li>${user.id}/${user.name}/${user.age}</li>`
     })
@@ -14,18 +14,36 @@ const display = (users) => {
 const url =
   'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users'
 
+// 用箭頭函式的寫法
+// const getUsers= async ()=>{
+// }
+async function getUsers() {
+  // await關鍵字只能在async函式裡使用
+  // 等候promise實現or拒絕，回傳解析的值or理由
+  const res = await fetch(url)
+  // 等候res物件解析為js資料格式
+  const users = await res.json()
+  // 除錯
+  console.log(users)
+  // 呈現結果在頁面上
+  display(users)
+}
+
+// 呼叫執行
+getUsers()
+
 // method: get 獲取資料(預設)
-fetch(url)
-  .then((res) => {
-    // 從回應(res)物件解析json資料為js資料格式
-    return res.json()
-  })
-  .then((users) => {
-    console.log(users)
-    // 要在promise的then的callback裡面呼叫呈現的函式
-    display(users)
-  })
-  .catch((error) => {
-    // 錯誤處理用
-    console.error(error)
-  })
+// fetch(url)
+//   .then((res) => {
+//     // 從回應(res)物件解析json資料為js資料格式
+//     return res.json()
+//   })
+//   .then((users) => {
+//     console.log(users)
+//     // 要在promise的then的callback裡面呼叫呈現的函式
+//     display(users)
+//   })
+//   .catch((error) => {
+//     // 錯誤處理用
+//     console.error(error)
+//   })
